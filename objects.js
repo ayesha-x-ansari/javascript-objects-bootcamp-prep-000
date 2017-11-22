9e8f63f3785fea47e9c21e56d545619b4cd0ff4b
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-11-22 20:24:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   'Mariah Carey': 'Oh Holy Night',
   Rihanna: 'Take a bow'
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = songTitle
+  return playlist
+}
