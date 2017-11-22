cd02be0fa314ce825e15bf993efb9dc40d9e4bbd
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-11-22 20:28:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,9 @@ var playlist = {
   Rihanna: 'Take a bow'
 }
 
+var artistName = 'Phil Ochs'
+var songTitle  = "Here's to the State of Mississippi"
+
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist.artistName = songTitle
   return playlist
